4c81b021988fe3ba84a8a310dfd35e7bd100c74a
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2022-08-08 18:42:25 -0500

    Implemented post data retrieval and display

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import PostList from "./components/PostList";
 function App() {
   return (
     <div className="fixed flex h-screen w-screen bg-gray-500">
-        <div className="absolute flex left-1/4 h-screen w-1/2 bg-gray-600">
+        <div className="relative flex left-1/4 h-screen w-1/2 bg-gray-600">
             <Menu />
             <PostList />
         </div>
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,15 +1,34 @@
 import {Component} from "react";
 
-class PostList extends Component<{ }, any>{
+class PostList extends Component<{ }, {posts: any}>{
     constructor(props: any) {
         super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/api/posts')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({posts: data})
+            })
     }
 
     render() {
         return (
-            <div className="relative top-[35%] left-[13.5%] h-1/5 w-[73%] m-0 flex flex-col bg-gray-800">
+            <div className="absolute top-[35%] left-[13.5%] w-[72%]">
+                {this.state.posts.map((post: {postId: number, title: string, author: string, content: string, date: string, likes: number}) => (
+                    <div className="relative mb-4 flex flex-col bg-gray-800 text-white">
+                        <h1 className="relative text-2xl mt-2 ml-3">{post.title}</h1>
+                        <text className="relative mt-1 ml-3 text-gray-400">{'By: ' + post.author}</text>
+                        <text className="relative mt-4 ml-3">{post.content}</text>
+                        <text className="relative top-[calc(100%-96px)] left-[calc(100%-96px)]">{post.date}</text>
+                        <text className="relative top-[calc(100%-96px)] left-[calc(100%-96px)] mb-3">{/*'Likes: ' + post.likes*/}</text>
 
-
+                    </div>
+                ))}
             </div>
         );
     }
